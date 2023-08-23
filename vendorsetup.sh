532d994fa7fee0aa4f2abc29f9009e584c77a9f2
AdarshGrewal <adarshgrewal@gmail.com>
2023-08-23 14:35:26 +0300

    haydn: Include Leica Camera if exists

--- vendorsetup.sh
+++
@@ -1,7 +0,0 @@
-echo 'Starting to clone stuffs needed to build for Haydn'
-
-# Firmware
-echo 'Cloning firmware'
-git clone --depth=1 https://gitlab.com/Alucard_Storm/vendor_xiaomi_haydn-firmware -b thirteen vendor/xiaomi/haydn-firmware
-
-echo 'delete vendorsetup.sh from device tree once this is done'
