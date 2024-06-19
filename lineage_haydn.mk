1678329993c32aebc44c7c59766347deb01322b8
1xtAsh <ashraykrsingh321@gmail.com>
2024-06-19 04:55:16 +0000

    haydn: @Los-Ext -Ify

--- lineage_haydn.mk
+++ lineage_haydn.mk
@@ -20,6 +20,9 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := M2012K11G
 PRODUCT_NAME := lineage_haydn
 
+# Gapps
+WITH_GMS := false
+
 PRODUCT_SYSTEM_NAME := haydn_global
 PRODUCT_SYSTEM_DEVICE := haydn
 
