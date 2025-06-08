273ebe63d6cc8c65c41e4b5e57398f4d070808a2
Hexdare <mohammedasimuddin786@gmail.com>
2025-06-08 22:08:44 +0000

    haydn: set camera override format from reserved via soong config

--- device.mk
+++ device.mk
@@ -54,6 +54,7 @@ $(call inherit-product, vendor/xiaomi/haydn/haydn-vendor.mk)
 
 # Call the Leica Camera setup
 $(call inherit-product-if-exists, vendor/xiaomi/haydn-miuicamera/products/miuicamera.mk)
+$(call soong_config_set,camera,override_format_from_reserved,true)
 
 # Call thr Bcr setup
 $(call inherit-product, vendor/bcr/bcr.mk)
