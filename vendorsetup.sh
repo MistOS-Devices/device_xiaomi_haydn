ffd0987cb4ca50d8b8ce0bcf3a47be5264570f76
Hexdare <mohammedasimuddin786@gmail.com>
2025-06-10 10:27:49 +0000

    haydn: Add vendorsetup.sh
    Signed-off-by: Hexdare <hexdare@outlook.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,51 @@
+echo "Starting to clone stuffs needed to build for Haydn"
+
+# Common device tree (chipset tree)
+if [ ! -d device/xiaomi/sm8350-common ]; then
+   git clone https://github.com/Haydn-Lab/device_xiaomi_sm8350-common -b 15 device/xiaomi/sm8350-common
+fi
+
+# Kernel
+if [ ! -d kernel/xiaomi/sm8350 ]; then
+   git clone --depth=1 https://github.com/Hexdare/ffs-kernel --recursive -b FarFromStock kernel/xiaomi/sm8350
+fi
+
+# Main vendor tree
+if [ ! -d vendor/xiaomi/haydn ]; then
+   git clone --depth=1 https://github.com/Haydn-Lab/vendor_xiaomi_haydn -b 15 vendor/xiaomi/haydn
+fi
+
+# Common vendor tree
+if [ ! -d vendor/xiaomi/sm8350-common ]; then
+   git clone --depth=1 https://github.com/Haydn-Lab/vendor_xiaomi_sm8350-common -b 15 vendor/xiaomi/sm8350-common
+fi
+
+# Firmware setup
+if [ ! -d vendor/xiaomi/haydn-firmware ]; then
+   git clone --depth=1 https://gitlab.com/Hexdare/vendor_xiaomi_haydn-firmware.git -b fourteen vendor/xiaomi/haydn-firmware
+fi
+
+# Xiaomi Leica Camera
+if [ ! -d vendor/xiaomi/haydn-miuicamera ]; then
+   git clone --depth=1 https://gitlab.com/Hexdare/haydn-miuicamera -b fifteen-leica vendor/xiaomi/haydn-miuicamera
+fi
+
+# Dolby
+if [ ! -d hardware/dolby ]; then
+   git clone --depth=1 https://github.com/Haydn-Lab/hardware_dolby -b 15 hardware/dolby
+fi
+
+# BCR (Basic Call Recorder)
+if [ ! -d vendor/bcr ]; then
+   git clone --depth=1 https://github.com/Chaitanyakm/vendor_bcr -b main vendor/bcr
+fi
+
+# Xiaomi hardware tree
+if [ ! -d hardware/xiaomi/* ]; then
+   git clone --depth=1 https://github.com/Evolution-X-Devices/hardware_xiaomi.git -b vic hardware/xiaomi
+fi
+
+rm -rf hardware/xiaomi/dolby
+rm -rf hardware/xiaomi/DSP*
+
+echo "Delete vendorsetup.sh from device tree once this is done."
